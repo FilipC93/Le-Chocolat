90800ec643acdc4fce0c92532ee0607f74650f49
FilipC <mfcekani@gmail.com>
2020-07-09 21:58:19 +0300

    Filip final Bootstrap project

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,7 @@ $(document).ready(function () {
         $("#mycarousel").carousel('cycle');
     });
 });
+$(document).ready(function() {
+    $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
+    $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
+});
